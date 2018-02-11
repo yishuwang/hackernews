c66eb98973ca4569df814f21f2c3b3e27c79dbd7
wangyishu <wangyishu@maxthon.net>
2018-02-11 16:34:19 +0800

    定义更多的变量然后再 JSX 中引用并渲染(render)它们

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var user1 = {
+      'firstName': 'little',
+      'lastName': 'sister'
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello, {user1.firstName + ' ' + user1.lastName}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
