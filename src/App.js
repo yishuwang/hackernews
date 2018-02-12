e5b5788d189ec68d11b40d68a23d93cf21f08453
wangyishu <wangyishu@maxthon.net>
2018-02-12 15:49:14 +0800

    用模拟数据，展示一个列表

--- src/App.js
+++ src/App.js
@@ -2,18 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// react拥抱不可变
+const user1 = {
+  firstName: '健康',
+  lastName: '平安'
+}
+// 模拟API获取到数据
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0 // key属性，列表变化时识别成员状态，提升性能
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1
+  }
+];
+
 class App extends Component {
   render() {
-    const user1 = {
-      'firstName': 'little',
-      'lastName': 'sister'
-    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, {user1.firstName + ' ' + user1.lastName}</h1>
+          <h1 className="App-title">Hello, 祝你{user1.firstName + ' ' + user1.lastName}</h1>
         </header>
+        <p>确保列表每个成员的关键字key属性是稳定的标识符，而不是使用不稳定的数组索引，唯一key帮助react识别具体成员的增删改，以提升性能。</p>
+        {list.map(function (item) {
+          return (
+            <div key={item.objectId}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
