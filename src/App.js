7387331616950f16a9b5ecff5e3d1430b70aca01
wangyishu <wangyishu@maxthon.net>
2018-02-12 23:13:36 +0800

    内部状态修改，组件重新渲染并展示正确数据

--- src/App.js
+++ src/App.js
@@ -26,8 +26,15 @@ const list = [
     objectId: 1
   }
 ];
-
+// 通过继承类的方式声明组件，公共接口render必须被重写，他定义了一个react组件的输出
 class App extends Component {
+  // 构造函数中初始化组件的状态  
+  constructor(props) {
+    super(props); // 会在构造函数中设置this.props以供在构造函数中访问他们
+    this.state = { //state使用this绑定在类上，整个组件可以访问到,每次修改组件内部状态，render会再次运行
+      list: list
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -48,12 +55,23 @@ class App extends Component {
             </div>
           );
         })}
-        <p>ES6 箭头函数
+        <p>ES6 箭头函数 省略了函数声明表达式、花括号和返回声明，JSX简洁可读。
             注意：普通函数表达式会定义自己的this对象，但箭头函数仍使用包含它的语境下的this对象；
             函数只有一个参数时，可以省去括号；
             简洁函数体替代块状函数体，简洁函数体的返回不用显示声明，可移除return表达式。
             </p>
         {list.map(item => 
+          <div key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )}
+        <p>此前在render方法中映射一个在组件外定义静态列表。现在可以在组件中使用state里的list了</p>
+        {this.state.list.map(item=>
           <div key={item.objectId}>
             <span>
               <a href={item.url}>{item.title}</a>
