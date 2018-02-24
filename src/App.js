99efb20d1949041012ddf55834d08dccb4dbcd29
wangyishu <wangyishu@maxthon.net>
2018-02-24 16:33:59 +0800

    优化成轻量的函数式无状态组件

--- src/App.js
+++ src/App.js
@@ -87,50 +87,35 @@ class App extends Component {
     );
   }
 }
-function Search({value, onChange,children}) {
-    return (
-      <form>
-        {children}<input type="text"
-          value={value}
-          onChange={onChange}/>
-      </form>
-    );
-}
-class Table extends Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item=>
-          <div key={item.objectId}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectId)}>
-                Dissmiss
-              </Button>
-            </span>
-          </div>
-        )}
+const Search = ({value, onChange, children}) => 
+  <form>
+    {children}<input type="text"
+      value={value}
+      onChange={onChange}/>
+  </form>
+const Table = ({list, pattern, onDismiss}) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item=>
+      <div key={item.objectId}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectId)}>
+            Dissmiss
+          </Button>
+        </span>
       </div>
-    )
-  }
-}
-class Button extends Component {
-  render() {
-    const {onClick,className='',children} = this.props;
-    return (
-      <button 
-        onClick = {onClick}
-        className = {className}
-        type="button">
-        {children}
-      </button>
-    );
-  }
-}
+    )}
+  </div>
+const Button = ({onClick, className='', children}) =>
+  <button 
+    onClick = {onClick}
+    className = {className}
+    type="button">
+    {children}
+  </button>
 export default App;
