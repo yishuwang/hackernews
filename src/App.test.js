46fe79d30c921cf60292c1b089eba302a7e63118
wangyishu <wangyishu@maxthon.net>
2018-03-04 22:41:10 +0800

    Enzyme单元测试

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App, {Search, Button, Table} from './App';
+
+Enzyme.configure({adapter: new Adapter()});
 
 describe('App', () => {
 	it('renders without crashing', () => {
@@ -45,7 +49,9 @@ describe('Button', () => {
 describe('Table', () => {
 	const props = {
 		list: [
-		{title: '1', author:'1',num_comments:1,points:2,objectID:'x'}],
+		{title: '1', author:'1',num_comments:1,points:2,objectID:'x'},
+		{title: '2', author:'2',num_comments:1,points:2,objectID:'y'},
+		],
 	};
 	it('renders without crashing', () => {
 		const div = document.createElement('div');
@@ -57,5 +63,11 @@ describe('Table', () => {
 		);
 		let tree = component.toJSON();
 		expect(tree).toMatchSnapshot();
-	})
+	});
+	it('shows two items in list', () => {
+	    const element = shallow(
+		<Table { ...props } />
+	);
+    expect(element.find('.table-row').length).toBe(2);
+  });
 });
