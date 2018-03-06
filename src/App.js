5fc5c49f03f2b1e306711b93cf7c13a6eec7e1ba
wangyishu <wangyishu@maxthon.net>
2018-03-06 17:22:10 +0800

    ref属性可以访问元素的节点 程序第一次渲染时input字段要聚焦

--- src/App.js
+++ src/App.js
@@ -148,13 +148,28 @@ class App extends Component {
     );
   }
 }
-const Search = ({value, onChange, onSubmit, children}) => 
-  <form onSubmit={onSubmit}>
-    <input type="text"
-      value={value}
-      onChange={onChange}/>
-      <button type="submit"> {children} </button>
-  </form>
+// ES6 this对象可通过ref引用DOM节点
+class Search extends Component {
+  componentDidMount() {
+    if(this.input) {
+      this.input.focus();
+    }
+  }
+  render() {
+    const {value, onChange, onSubmit, children} = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+      <input type="text"
+        value={value}
+        onChange={onChange}
+        ref={(node) => {this.input = node;}}
+      />
+        <button type="submit"> {children} </button>
+      </form>
+    )
+  }
+}
+ 
 const Table = ({list, onDismiss}) =>
   <div className="table">
     {list.map(item=>
